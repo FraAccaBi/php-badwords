254cee83438b37e642ec63c55abf05dc07fc5f69
FraAccaBi <francescahuda@gmail.com>
2022-05-19 14:34:38 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+$paragraph = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident impedit, quas maxime illum in molestias at voluptatem quidem reiciendis odio autem numquam vero, quisquam excepturi nisi fuga ipsam est facere.'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php echo $paragraph ?>
+    </p>
+</body>
+</html>
